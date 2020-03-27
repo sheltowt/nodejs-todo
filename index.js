3b4381d93ef99877237736dba57597d1960cbb89
new_account <sheltowt@gmail.com>
2020-03-27 14:45:29 -0600

    adding modifications for my custom app

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 //dependencies required for the app
 var express = require("express");
+var cookieParser = require('cookie-parser');
+var cookieEncrypter = require('./cook');
 var bodyParser = require("body-parser");
 var app = express();
+
+var config = require('./config.json');
+
+var secretKey = config["secret_key"];
+
+app.use(cookieParser(secretKey));
+app.use(cookieEncrypter(secretKey));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
@@ -9,19 +18,26 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 //placeholders for added task
-var task = ["buy socks", "practise with nodejs"];
+var task = ["create cloudflare worker"];
 //placeholders for removed task
-var complete = ["finish jquery"];
+var complete = ["set up TLS"];
 
 //post route for adding new task 
 app.post("/addtask", function(req, res) {
+    console.log("adding task")
     var newTask = req.body.newtask;
     //add the new task from the post route
     task.push(newTask);
+    res.cookie('should_work', "true", {
+      maxAge: 900000,
+      signed: true,
+      httpOnly: true
+    });
     res.redirect("/");
 });
 
 app.post("/removetask", function(req, res) {
+    console.log("removing task")
     var completeTask = req.body.check;
     //check for the "typeof" the different completed task, then add into the complete task
     if (typeof completeTask === "string") {
@@ -34,7 +50,32 @@ app.post("/removetask", function(req, res) {
             task.splice(task.indexOf(completeTask[i]), 1);
         }
     }
+    res.cookie('should_work', "false", {
+      maxAge: 900000,
+      signed: true,
+      httpOnly: true
+    });
     res.redirect("/");
+});
+
+app.get("/entertainment", function(req, res) {
+    console.log("shouldWork")
+    console.log("entertainment")
+    console.log(req.signedCookies)
+    if (req.signedCookies.should_work) {
+        var shouldWork = new Buffer(req.signedCookies.should_work).toString();
+        console.log(shouldWork)
+        if (shouldWork == "false") {
+            console.log("redirecting")
+            res.redirect(307, "https://www.youtube.com/watch?v=XyNlqQId-nk");
+        } else {
+          console.log("no redirect")
+          res.render("index", { task: task, complete: complete });
+        }      
+    } else {
+        console.log("no cookie")
+        res.render("index", { task: task, complete: complete });         
+    }
 });
 
 //render the ejs and display added task, completed task
